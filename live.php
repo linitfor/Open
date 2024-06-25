5f707c45d0204073fda3d2a653b531343b28ff3f
linitfor <58168507+linitfor@users.noreply.github.com>
2024-06-25 18:13:04 +0800

    Update live.php

--- live.php
+++ live.php
@@ -1,1 +1,7 @@
-{"code":1,"msg":200,"class":[{"type_id":"台灣","type_name":"\u592e\u89c6\u9891\u9053"},{"type_id":"5Y2r6KeG6aKR6YGT","type_name":"\u536b\u89c6\u9891\u9053"},{"type_id":"5pWw5a2X6aKR6YGT","type_name":"\u6570\u5b57\u9891\u9053"},{"type_id":"5LiK5rW36aKR6YGT","type_name":"\u4e0a\u6d77\u9891\u9053"},{"type_id":"5rWZ5rGf6aKR6YGT","type_name":"\u6d59\u6c5f\u9891\u9053"},{"type_id":"5YaF6JKZ6aKR6YGT","type_name":"\u5185\u8499\u9891\u9053"}]}
+{"code":1,"msg":200,"class":[
+{"type_id":"Taiwan","type_name":"Taiwan"},
+{"type_id":"5Y2r6KeG6aKR6YGT","type_name":"\u536b\u89c6\u9891\u9053"},
+{"type_id":"5pWw5a2X6aKR6YGT","type_name":"\u6570\u5b57\u9891\u9053"},
+{"type_id":"5LiK5rW36aKR6YGT","type_name":"\u4e0a\u6d77\u9891\u9053"},
+{"type_id":"5rWZ5rGf6aKR6YGT","type_name":"\u6d59\u6c5f\u9891\u9053"},
+{"type_id":"5YaF6JKZ6aKR6YGT","type_name":"\u5185\u8499\u9891\u9053"}]}
